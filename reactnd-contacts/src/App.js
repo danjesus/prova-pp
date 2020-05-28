dc0e15103124d1cdc6dcbb5a7e814cf0eb931e4a
Dan Jesus <danper.jesus@gmail.com>
2020-05-28 01:41:04 -0300

    Fix docker server build and configs

--- reactnd-contacts/src/App.js
+++ reactnd-contacts/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
 
   render() {
     return (
-      <div> 
+      <div>
         <Route path='/list' render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
--- reactnd-contacts/src/loginPage/index.js
+++ reactnd-contacts/src/loginPage/index.js
@@ -1,29 +1,24 @@
-import React,{Component} from 'react';
+import React,{Component} from 'react'
 
+export default class loginPage extends Component {
+    render() {
+      return (
+          <body>
+              <div className ="login">
+                  <img src="/images/profile.png" className="usuario" width="100" height="100" alt="" />
+                  <h1>AD PHONEBOOK</h1>
+                  <form>
 
+                      <p>Usuário</p>
+                      <input type="text" name="" placeholder="Usuário ou Email" required />
+                      <p>Senha</p>
+                      <input type="password" name="" placeholder="Insira sua senha" required />
+                      <input type="submit" name="" value="Entrar" onClick={() => window.redirect('/list')}/>
+                      <a href="/List">Esqueceu sua senha?</a>
 
-
-
-export default class loginPage extends Component { 
-    render (){
-
-    return (
-        <body>
-            <div className ="login">
-                <img src="/images/profile.png" className="usuario" width="100" height="100" alt="" />
-                <h1>AD PHONEBOOK</h1>
-                <form>
-
-                    <p>Usuário</p>
-                    <input type="text" name="" placeholder="Usuário ou Email" required />
-                    <p>Senha</p>
-                    <input type="password" name="" placeholder="Insira sua senha" required />
-                    <input type="submit" name="" value="Entrar" onClick={() => this.history.push('/list')}/>
-                    <a href="/List">Esqueceu sua senha?</a>
-
-                </form>
-            </div>
-        </body>
-    );
+                  </form>
+              </div>
+          </body>
+      )
     }
 }
